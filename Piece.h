59cc9538909b84160c835736124a1d3ecacd014c
Zenda <zenda.co@proton.me>
2026-01-17 21:14:03 +0100

    Add Piece class with basic functionality

--- Piece.h
+++ Piece.h
@@ -1,1 +1,20 @@
+#pragma once
 
+class Player;
+class Spot;
+class Piece {
+public:
+    explicit Piece(Player* owner = nullptr);
+    Player* getOwner() const;
+    Spot* getPosition() const;
+    bool isPlaced() const;
+    bool isInMill() const;
+    void place(Spot* newPosition);
+    void removeFromBoard();
+    void setMillStatus(bool inMill);
+
+private:
+    Player* owner_; 
+    Spot* position_;
+    bool inMill_;       
+}; 
