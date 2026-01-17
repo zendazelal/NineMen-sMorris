ab8529ae13f914ffa53c17db2ee8f3035571c786
Zenda <zenda.co@proton.me>
2026-01-17 21:15:42 +0100

    Define Player class with member functions and variables

--- Player.h
+++ Player.h
@@ -1,1 +1,35 @@
+#pragma once
 
+#include <vector>
+#include <string>
+#include "Piece.h"
+#include "Spot.h"
+
+class Player {
+public:
+    Player(const std::string& name, int color);
+
+    std::string getName() const;
+    int getColor() const;
+
+    bool canFly() const; 
+    int activePieces() const; 
+
+    int availableToPlace() const;
+    int totalActivePieces() const; 
+
+    bool placePiece(Spot* spot); 
+    bool capturePiece(Piece* target); 
+
+    const std::vector<Piece>& getPieces() const; 
+    std::vector<Piece>& getPieces(); 
+
+    void incrementCaptured();
+    int getCaptured() const;
+
+private:
+    std::string name_;
+    int color_;
+    std::vector<Piece> pieces_;
+    int capturedPieces_ = 0; 
+};
